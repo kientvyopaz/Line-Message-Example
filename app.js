dcdf196ed1df6c92957b09f0f80af42c9f0c9e5c
Kien Van Tran <65330689+pipiakrt@users.noreply.github.com>
2021-11-12 15:08:32 +0700

    push code

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const middleware = require('@line/bot-sdk').middleware
+const Client = require('@line/bot-sdk').Client;
+const io = new Server(server);
+
+const config = {
+    channelAccessToken: process.env.LINE_BOOT_TOKEN || null,
+    channelSecret: process.env.LINE_BOOT_SECRET || null
+}
+
+io.on('connection', (socket) => {
+    socket.on('replyMessage', ({ replyToken, message }) => {
+        const client = new Client(config);
+        client.replyMessage(replyToken, {
+            type: 'text',
+            text: message,
+        });
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/webhook', middleware(config), (req, res) => {
+    const event = req.body.events[0];
+    io.emit('getMessage', event);
+})
+
+server.listen(process.env.PORT, () => {
+    console.log(`listening on *:${process.env.PORT}`);
+});
